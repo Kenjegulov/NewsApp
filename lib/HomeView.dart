cf028f26a12a41928f58f1f1f98ab3e96f7c41eb
Kenjegulov <arsen.kenjegulov.bj@gmail.com>
2023-05-30 22:48:47 +0600

    list news

--- lib/HomeView.dart
+++ lib/HomeView.dart
@@ -19,34 +19,27 @@ class _HomeViewState extends State<HomeView> {
   TopNews? topNews;
   final http.Client client = http.Client();
 
+  List<TopNews> jsonResponsies = [];
+
+  int items = 0;
+
   void newsData() async {
-    // Dio dio = Dio();
-    // final response = await dio.get(MyApi.myApiUrl);
     final Uri url = Uri.parse(MyApi.myApiUrl);
     final http.Response response = await client.get(url);
 
     if (response.statusCode == 200 || response.statusCode == 201) {
       Map<String, dynamic> jsonResponse =
           convert.jsonDecode(response.body) as Map<String, dynamic>;
-      // print(jsonResponse["articles"].length);
-      setState(() {
-        topNews = TopNews.fromJson(jsonResponse, 1);
-        // topNews = TopNews(
-        //   status: response.data["status"],
-        //   totalResults: response.data["totalResults"],
-        //   articles: Article(
-        //       source: Source(
-        //           id: response.data["articles"][1]["source"]["id"],
-        //           name: response.data["articles"][1]["source"]["name"]),
-        //       author: response.data["articles"][1]["author"],
-        //       title: response.data["articles"][1]["title"],
-        //       description: response.data["articles"][1]["description"],
-        //       url: response.data["articles"][1]["url"],
-        //       urlToImage: response.data["articles"][1]["urlToImage"],
-        //       publishedAt: response.data["articles"][1]["publishedAt"],
-        //       content: response.data["articles"][1]["content"]),
-        // );
-      });
+      items = jsonResponse["articles"].length;
+
+      for (int i = 0; i < 2; i++) {
+        setState(() {
+          topNews = TopNews.fromJson(jsonResponse, i);
+          print(topNews);
+          print("Arsen");
+          jsonResponsies.add(topNews!);
+        });
+      }
     }
   }
 
@@ -59,40 +52,52 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Tapshyrma 10')),
-      ),
-      body: Column(
-        children: [
-          Text("${topNews?.status}"),
-          Text("${topNews?.totalResults}"),
-          Text("${topNews?.articles.source.id}"),
-          Text("${topNews?.articles.source.name}"),
-          Text("${topNews?.articles.author}"),
-          Text("${topNews?.articles.title}"),
-          Text("${topNews?.articles.description}"),
-          GestureDetector(
-            onTap: () async {
-              final Uri url = Uri.parse("${topNews?.articles.url}");
-              if (await canLaunchUrl(url)) {
-                await launchUrl(url);
-              } else {
-                throw 'Could not launch $url';
-              }
-            },
-            child: Text(
-              "${topNews?.articles.title}",
-              style: const TextStyle(
-                  color: Colors.blue,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w100),
-            ),
+        appBar: AppBar(
+          title: Center(child: Text('Tapshyrma 10 $items')),
+        ),
+        body: ListView.builder(
+          itemCount: items,
+          prototypeItem: ListTile(
+            title: Text("$items"),
           ),
-          Image(image: NetworkImage("${topNews?.articles.urlToImage}")),
-          Text("${topNews?.articles.publishedAt}"),
-          Text("${topNews?.articles.content}"),
-        ],
-      ),
-    );
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Column(
+                children: [
+                  Text(jsonResponsies[index].status),
+                  Text("${jsonResponsies[index].totalResults}"),
+                  Text("${jsonResponsies[index].articles.source.id}"),
+                  Text(jsonResponsies[index].articles.source.name),
+                  Text("${jsonResponsies[index].articles.author}"),
+                  Text(jsonResponsies[index].articles.title),
+                  Text(jsonResponsies[index].articles.description),
+                  GestureDetector(
+                    onTap: () async {
+                      final Uri url =
+                          Uri.parse(jsonResponsies[index].articles.url);
+                      if (await canLaunchUrl(url)) {
+                        await launchUrl(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                    child: Text(
+                      jsonResponsies[index].articles.title,
+                      style: const TextStyle(
+                          color: Colors.blue,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w100),
+                    ),
+                  ),
+                  Image(
+                      image: NetworkImage(
+                          "${jsonResponsies[index].articles.urlToImage}")),
+                  Text(jsonResponsies[index].articles.publishedAt),
+                  Text(jsonResponsies[index].articles.content),
+                ],
+              ),
+            );
+          },
+        ));
   }
 }
